3698945507a3ec53115bfeb3fa155f6e3468dd33
Oded Kesler <odedkesler1@mail.tau.ac.il>
2022-06-17 21:03:14 +0300

    optimized aqp

--- AQPHashTable.java
+++ AQPHashTable.java
@@ -1,15 +1,17 @@
 
 public class AQPHashTable extends OAHashTable {
 	ModHash hashParams;
+	int direction;
 
 	public AQPHashTable(int m, long p) {
 		super(m);
+		this.direction = -1;
 		this.hashParams = ModHash.GetFunc(m, p);
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
-		int direction = (int) Math.pow(-1,i);
+		direction = direction* -1;
 		return Math.floorMod(this.hashParams.Hash(x) + (direction*(i*i)), this.size);
 	}
 	
--- McTester.java
+++ McTester.java
@@ -6,32 +6,38 @@ public class McTester {
 		int m = 10000019;
 		long p = 1000000007;
 		int n = 9500018;
+		int fail_counter = 0;
 		
 		Random random = new Random();
 		int i = 0;
 		HashTableElement[] elements = new HashTableElement[n]; 
 		
-		for (; i<n; i++) {
-	
-			long b = random.nextLong(100);
-			long number = 100*i + b;
-			elements[i] = new HashTableElement(number, number);
-		}
-//		
-//		AQPHashTable aqp = new AQPHashTable(m,p);
-//		int j = 0;
-//		for (;j<m; j++) {
-//			try {
-//				aqp.Insert(elements[j]);
+//		int x = 0;
+//		while (x < 100) {
+//			for (; i<m; i++) {
+//				
+//				long b = random.nextLong(100);
+//				long number = 100*i + b;
+//				elements[i] = new HashTableElement(number, number);
 //			}
-//			catch (IHashTable.TableIsFullException e) {
-//				System.out.println("Table is full at " + j + " insertion");
-//				break;
+//			
+//			OAHashTable qp = new AQPHashTable(m,p);
+//			int j = 0;
+//			for (;j<m; j++) {
+//				try {
+//					qp.Insert(elements[j]);
+//				}
+//				catch (IHashTable.TableIsFullException e) {
+//					fail_counter++;
+//					System.out.println("Iteraion " + x + ": Table is full at " + j + " insertion");
+//					break;
+//				}
 //			}
+//			x++;
 //		}
+//		System.out.println("fail counter is: " + fail_counter);
 		
 //		DoubleHashTable dh = new DoubleHashTable(m,p);
-//		Random random = new Random();
 //		int x = 1;
 //		
 //		for (; x < 7; x++) {
@@ -74,18 +80,25 @@ public class McTester {
 //		}
 //		System.out.println("Done");		
 		
-//		LPHashTable lp = new LPHashTable(m, p); 
-//		QPHashTable qp = new QPHashTable(m, p);
-//		AQPHashTable aqp = new AQPHashTable(m, p);
-//		DoubleHashTable dh = new DoubleHashTable(m, p); 
-//		
-//		long startTime = System.nanoTime();
-//		for (int j = 0; j < n; j++) {
-//			lp.Insert(elements[j]);
-//		}
-//		long stopTime = System.nanoTime();
-//		long executionTime = (stopTime - startTime)/1000000;
-//		System.out.println("LP execution time in s*1000: " + executionTime);
+		OAHashTable lp = new LPHashTable(m, p); 
+		OAHashTable qp = new QPHashTable(m, p);
+		OAHashTable aqp = new AQPHashTable(m, p);
+		OAHashTable dh = new DoubleHashTable(m, p); 
+		
+		for (; i<n; i++) {
+			
+			long b = random.nextLong(100);
+			long number = 100*i + b;
+			elements[i] = new HashTableElement(number, number);
+		}
+		
+		long startTime = System.nanoTime();
+		for (int j = 0; j < n; j++) {
+			lp.Insert(elements[j]);
+		}
+		long stopTime = System.nanoTime();
+		long executionTime = (stopTime - startTime)/1000000;
+		System.out.println("LP execution time in s*1000: " + executionTime);
 		
 		
 //		startTime = System.nanoTime();
@@ -97,21 +110,22 @@ public class McTester {
 //		System.out.println("QP execution time in s*1000: " + executionTime);
 		
 		
-//		startTime = System.nanoTime();
-//		for (int j = 0; j < n; j++) {
-//			aqp.Insert(elements[j]);
-//		}
-//		stopTime = System.nanoTime();
-//		executionTime = (stopTime - startTime)/1000000;
-//		System.out.println("AQP execution time in s*1000: " + executionTime);
-//		
-//		
-//		startTime = System.nanoTime();
-//		for (int j = 0; j < n; j++) {
-//			dh.Insert(elements[j]);
-//		}
-//		stopTime = System.nanoTime();
-//		executionTime = (stopTime - startTime)/1000000;
-//		System.out.println("DH execution time in s*1000: " + executionTime);
+		startTime = System.nanoTime();
+		for (int j = 0; j < n; j++) {
+			aqp.Insert(elements[j]);
+		}
+		stopTime = System.nanoTime();
+		executionTime = (stopTime - startTime)/1000000;
+		System.out.println("AQP execution time in s*1000: " + executionTime);
+		
+		
+		startTime = System.nanoTime();
+		for (int j = 0; j < n; j++) {
+			dh.Insert(elements[j]);
+		}
+		stopTime = System.nanoTime();
+		executionTime = (stopTime - startTime)/1000000;
+		System.out.println("DH execution time in s*1000: " + executionTime);
+		System.out.println("Done");
 	}
 }
